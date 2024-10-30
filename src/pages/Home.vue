9ea4cbfb0a7d77a205ffae9a5b3659e04833eb43
Maxim Zaividy <m.zavidiy@consult-info.ru>
2024-10-30 15:20:19 +0300

    first_commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <section>
+    <div class="intro">
+      <div class="intro-title">
+        <h1>Мои заметки</h1>
+        <h3>Не забывай о важном, храни его в облаке.</h3>
+      </div>
+      <picture>
+        <img src="/images/intro.png" alt="intro" />
+      </picture>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .intro {
+    max-width: 1724px;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    picture {
+      width: 60%;
+      height: 60%;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .intro-title {
+    h1 {
+      font-size: 6.5rem;
+      font-weight: 900;
+      color: #FFFFFF;
+      line-height: 6.5rem;
+    }
+
+    h3 {
+      color: #9DA5AF;
+      font-size: 2rem;
+      width: 60%;
+      margin-top: 40px;
+    }
+  }
+</style>
