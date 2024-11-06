c3570243e38cb103802290ec35e62019fdca1e84
Maxim Zaividy <m.zavidiy@consult-info.ru>
2024-11-06 18:30:49 +0300

    Настройка чеклиста WIP

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,58 @@
 <script setup>
 import Logo from "../assets/svg/Logo.vue";
 import {router} from "../router/index.js";
+import {useRoute} from "vue-router";
+import User from "../assets/svg/User.vue";
+import {ref} from "vue";
+
+const route = useRoute();
+
 function goToChecklist() {
   router.push('checklist');
+}
+function goToHome() {
+  router.push('/');
+}
+const showButton = ref(false);
+let timeout;
+
+function handleMouseEnter() {
+  showButton.value = true;
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+}
+function handleMouseLeave() {
+  timeout = setTimeout(() => {
+    showButton.value = false;
+  }, 500);
 }
 </script>
 
 <template>
   <header>
     <Logo />
-    <button
-      @click="goToChecklist"
-    >
-      <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6.3079 4.54487C5.90926 4.92711 5.89596 5.56013 6.2782 5.95877C6.66044 6.35741 7.29347 6.3707 7.6921 5.98846L6.3079 4.54487ZM7.6921 28.0115C7.29347 27.6293 6.66044 27.6426 6.2782 28.0412C5.89596 28.4399 5.90926 29.0729 6.3079 29.4551L7.6921 28.0115ZM1 16C0.447715 16 0 16.4477 0 17C0 17.5523 0.447715 18 1 18V16ZM23 17L23.7071 17.7071C24.0976 17.3166 24.0976 16.6834 23.7071 16.2929L23 17ZM16.2929 22.2929C15.9024 22.6834 15.9024 23.3166 16.2929 23.7071C16.6834 24.0976 17.3166 24.0976 17.7071 23.7071L16.2929 22.2929ZM17.7071 10.2929C17.3166 9.90237 16.6834 9.90237 16.2929 10.2929C15.9024 10.6834 15.9024 11.3166 16.2929 11.7071L17.7071 10.2929ZM32 17C32 25.3158 25.4872 32 17.5227 32V34C26.6539 34 34 26.3573 34 17H32ZM17.5227 2C25.4872 2 32 8.68416 32 17H34C34 7.64273 26.6539 0 17.5227 0V2ZM7.6921 5.98846C10.2781 3.50887 13.7316 2 17.5227 2V0C13.1867 0 9.2448 1.72879 6.3079 4.54487L7.6921 5.98846ZM17.5227 32C13.7316 32 10.2781 30.4911 7.6921 28.0115L6.3079 29.4551C9.2448 32.2712 13.1867 34 17.5227 34V32ZM1 18H23V16H1V18ZM22.2929 16.2929L16.2929 22.2929L17.7071 23.7071L23.7071 17.7071L22.2929 16.2929ZM23.7071 16.2929L17.7071 10.2929L16.2929 11.7071L22.2929 17.7071L23.7071 16.2929Z" fill="white"/>
-      </svg>
-      Вперед
-    </button>
+    <template v-if="route.name === 'home'">
+      <button
+          @click="goToChecklist"
+      >
+        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M6.3079 4.54487C5.90926 4.92711 5.89596 5.56013 6.2782 5.95877C6.66044 6.35741 7.29347 6.3707 7.6921 5.98846L6.3079 4.54487ZM7.6921 28.0115C7.29347 27.6293 6.66044 27.6426 6.2782 28.0412C5.89596 28.4399 5.90926 29.0729 6.3079 29.4551L7.6921 28.0115ZM1 16C0.447715 16 0 16.4477 0 17C0 17.5523 0.447715 18 1 18V16ZM23 17L23.7071 17.7071C24.0976 17.3166 24.0976 16.6834 23.7071 16.2929L23 17ZM16.2929 22.2929C15.9024 22.6834 15.9024 23.3166 16.2929 23.7071C16.6834 24.0976 17.3166 24.0976 17.7071 23.7071L16.2929 22.2929ZM17.7071 10.2929C17.3166 9.90237 16.6834 9.90237 16.2929 10.2929C15.9024 10.6834 15.9024 11.3166 16.2929 11.7071L17.7071 10.2929ZM32 17C32 25.3158 25.4872 32 17.5227 32V34C26.6539 34 34 26.3573 34 17H32ZM17.5227 2C25.4872 2 32 8.68416 32 17H34C34 7.64273 26.6539 0 17.5227 0V2ZM7.6921 5.98846C10.2781 3.50887 13.7316 2 17.5227 2V0C13.1867 0 9.2448 1.72879 6.3079 4.54487L7.6921 5.98846ZM17.5227 32C13.7316 32 10.2781 30.4911 7.6921 28.0115L6.3079 29.4551C9.2448 32.2712 13.1867 34 17.5227 34V32ZM1 18H23V16H1V18ZM22.2929 16.2929L16.2929 22.2929L17.7071 23.7071L23.7071 17.7071L22.2929 16.2929ZM23.7071 16.2929L17.7071 10.2929L16.2929 11.7071L22.2929 17.7071L23.7071 16.2929Z" fill="white"/>
+        </svg>
+        Вперед
+      </button>
+    </template>
+    <template v-else-if="route.name === 'checklist'">
+      <div
+          class="header-user"
+          @mouseenter="handleMouseEnter"
+          @mouseleave="handleMouseLeave"
+      >
+        <span>admin</span>
+        <User />
+        <a @click="goToHome" v-show="showButton"><span>На главную</span></a>
+      </div>
+    </template>
   </header>
 </template>
 
@@ -28,7 +64,53 @@ function goToChecklist() {
     display: flex;
     align-items: center;
     justify-content: space-between;
+  }
 
+  .header-user {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 20px;
+    color: #9DA5AF;
+    cursor: pointer;
+    position: relative;
+
+    a {
+      position: absolute;
+      bottom: -90px;
+      background: rgba(27, 47, 70, 1);
+      color: rgba(177, 201, 9, 1);
+      padding: 30px 0;
+      font-size: 18px;
+      width: 180px;
+      text-align: center;
+      border-radius: 20px;
+      box-shadow: 1px 10px 15px black;
+      transition: 0.3s;
+
+      &::before {
+        content: '';
+        position: absolute;
+        top: -5px;
+        right: 65px;
+        width: 20px;
+        height: 20px;
+        transform: rotate(45deg);
+        background: rgba(27, 47, 70, 1);
+      }
+
+      span {
+        display: inline-block;
+        transition: transform 0.3s;
+      }
+
+      &:hover {
+        span {
+          color: white;
+          transform: scale(1.1);
+        }
+      }
+    }
   }
 
 </style>
--- src/pages/Checklist.vue
+++ src/pages/Checklist.vue
@@ -1,11 +1,39 @@
 <script setup>
 
+import Header from "../components/Header.vue";
 </script>
 
 <template>
-
+  <Header />
+  <button class="checklist__add">
+    <span></span>
+    <span></span>
+  </button>
 </template>
 
 <style scoped>
+  .checklist__add {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 1px 11px 50px black;
 
+    span {
+      position: absolute;
+      width: 40px;
+      height: 4px;
+      background: #ffffff;
+      transform: rotate(90deg);
+      border-radius: 5px;
+
+      &:nth-of-type(1) {
+        transform: rotate(180deg);
+      }
+    }
+  }
 </style>
